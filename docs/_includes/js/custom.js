f637f00ec9a35e7fcd4ac466d03075f21b92d5e3
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-05-11 15:01:10 +0200

    update - dark-mode button now changes the text according to the state.

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -1,13 +1,3 @@
-var isClicked = false;
-async function toggleDetect() {
-  if (isClicked == false) {
-    isClicked = true;
-  } else {
-    isClicked = false;
-  }
-  await switchScheme(isClicked);
-}
-
 async function switchScheme(isClicked) {
   if (isClicked == true) {
     jtd.setTheme("openair");
